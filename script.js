0237f71a0e0b6438090914cf7580877413ce9e89
RobunGid <robungidorig@gmail.com>
2025-10-23 00:39:38 +0300

    first section

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { animate, scroll, hover } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
+import { animate, hover } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
 
 
 hover(".header--logo-container", (element) => {
@@ -10,4 +10,12 @@ hover(".header--logo-container", (element) => {
 hover(".header--navbar-item", (element) => {
 	animate(element, {scale: 1.05,}, { type: "spring"})
 	return () => animate(element, { scale: 1}, { type: "spring" })
+})
+
+animate('.about-photo', { y: [0, -5, 0], rotate: [0, -2, 2, 0], scale: [1, 1.05, 0.95, 1] }, {
+  duration: 3,
+  easing: 'spring',
+  stiffness: 300,
+  damping: 10,
+  repeat: Infinity
 })
