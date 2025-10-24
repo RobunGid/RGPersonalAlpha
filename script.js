48cb92dc21278ac41f34181c7b3aeb335fc938cb
RobunGid <robungidorig@gmail.com>
2025-10-25 00:48:23 +0300

    add cards

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import { animate, hover, inView } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
 
-
 hover(".header--logo-container", (element) => {
     animate(element, { scale: 1.1, rotate: 7 }, { type: "spring" })
 
