99d30e05a879321b4e0589d9cd890ef57e28e71e
RobunGid <robungidorig@gmail.com>
2025-10-26 05:43:20 +0300

    add stats experience skills

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 import { animate, hover, inView } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
 
 hover(".header--logo-container", (element) => {
-    animate(element, { scale: 1.1, rotate: 7 }, { type: "spring" })
+    animate(element, { scale: 1.1, rotate: 7 }, { type: "spring" });
 
-    return () => animate(element, { scale: 1, rotate: 0 }, { type: "spring" })
-})
+    return () => animate(element, { scale: 1, rotate: 0 }, { type: "spring" });
+});
 
 hover(".stats--item", (element) => {
-    animate(element.querySelectorAll(".stats--icon, .stats--title"), { scale: 1.1, y: -10 }, { type: "spring" })
+    animate(element.querySelectorAll(".stats--icon, .stats--title, .stats--subtitle"), { scale: 1.1, y: -5 }, { type: "spring" });
 
-    return () => animate(element.querySelectorAll(".stats--icon, .stats--title"), { scale: 1, y: 0 }, { type: "spring" })
-})
+    return () => animate(element.querySelectorAll(".stats--icon, .stats--title, .stats--subtitle"), { scale: 1, y: 0 }, { type: "spring" });
+});
+
+hover(".about--item", (element) => {
+    animate(element.querySelectorAll(".about--icon"), { scale: 1.1, y: -5, rotate: 7 }, { type: "spring" });
+
+    return () => animate(element.querySelectorAll(".about--icon"), { scale: 1, y: 0, rotate: 0 }, { type: "spring" });
+});
 
 hover(".header--navbar-item", (element) => {
-	animate(element, {scale: 1.05,}, { type: "spring"})
-	return () => animate(element, { scale: 1}, { type: "spring" })
-})
+	animate(element, {scale: 1.05,}, { type: "spring"});
+	return () => animate(element, { scale: 1}, { type: "spring" });
+});
+
+hover(".about--list-item", (element) => {
+	animate(element, {scale: 1.03,}, { type: "spring"});
+	animate(element.querySelector(".about--inline-icon"), {scale: 1.05,}, { type: "spring"});
+	return () => {
+		animate(element, { scale: 1}, { type: "spring" });
+		animate(element.querySelector(".about--inline-icon"), {scale: 1,}, { type: "spring"});
+};
+});
 
 animate('.about-photo', { y: [0, -5, 0], rotate: [0, -2, 2, 0], scale: [1, 1.05, 0.95, 1] }, {
   duration: 3,
@@ -23,7 +38,7 @@ animate('.about-photo', { y: [0, -5, 0], rotate: [0, -2, 2, 0], scale: [1, 1.05,
   stiffness: 300,
   damping: 10,
   repeat: Infinity
-})
+});
 
 const headingNameElement = document.querySelector("#heading-name");
 const text = headingNameElement.dataset.text;
