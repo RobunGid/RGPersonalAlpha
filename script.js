555526cacb082c71f3341c8db77a026dc866966e
RobunGid <robungidorig@gmail.com>
2025-10-27 00:21:16 +0300

    projects

--- script.js
+++ script.js
@@ -32,6 +32,13 @@ hover(".about--list-item", (element) => {
 };
 });
 
+hover(".projects--tech-item", (element) => {
+	animate(element, {scale: 1.03,}, { type: "spring"});
+	return () => {
+		animate(element, { scale: 1}, { type: "spring" });
+};
+});
+
 animate('.about-photo', { y: [0, -5, 0], rotate: [0, -2, 2, 0], scale: [1, 1.05, 0.95, 1] }, {
   duration: 3,
   easing: 'spring',
