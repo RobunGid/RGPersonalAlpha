201f7dd64d177c47db2f6ccb085dbb9c1b4e0f58
RobunGid <robungidorig@gmail.com>
2025-11-04 19:54:55 +0300

    main image animation fix

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 import { animate, hover, inView } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
 
+animate('.main--img', { y: [0, -5, 0], rotate: [0, -2, 2, 0], scale: [1, 1.05, 0.95, 1] }, {
+  duration: 3,
+  easing: 'spring',
+  stiffness: 300,
+  damping: 10,
+  repeat: Infinity
+});
+
 hover(".header--logo-container", (element) => {
     animate(element, { scale: 1.1, rotate: 7 }, { type: "spring" });
 
