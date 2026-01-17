881ddc1d99313852497450a5b1bd4c8e82e5da2d
RobunGid <robungidorig@gmail.com>
2026-01-17 22:34:48 +0300

    remove main image animation

--- frontend/script.js
+++ frontend/script.js
@@ -7,13 +7,13 @@ const MESSAGE_INPUT_MAX_LENGTH = 1280;
 
 const BACKEND_URL = "https://robgid.space/api/send"
 
-animate('.main--img', { y: [0, -5, 0], rotate: [0, -2, 2, 0], scale: [1, 1.05, 0.95, 1] }, {
-  duration: 3,
-  easing: 'spring',
-  stiffness: 300,
-  damping: 10,
-  repeat: Infinity
-});
+// animate('.main--img', { y: [0, -5, 0], rotate: [0, -2, 2, 0], scale: [1, 1.05, 0.95, 1] }, {
+//   duration: 3,
+//   easing: 'spring',
+//   stiffness: 300,
+//   damping: 10,
+//   repeat: Infinity
+// });
 
 hover(".header--logo-container", (element) => {
     animate(element, { scale: 1.1, rotate: 7 }, { type: "spring" });
